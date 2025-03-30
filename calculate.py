55648ca4f9ada8d4ccb110496e9db70748caf930
MuhmmadAyman <muhammad24ai@drttit.edu.in>
2025-03-30 17:51:11 +0530

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,6 @@
+n=int(input("enter the first number:"))
+s=int(input("enter seconf number:"))
+print("Addition of these numbers are",n+s)
+print("  subtraction of these numbers are",n-s)
+print("  multiplication of these numbers are",n*s)
+print("  division of these numbers are",n/s)
---
+++ concatenation.py
@@ -0,0 +1,4 @@
+n=input("enter ur first name:")
+s=input("enter ur second name:")
+concatenate=n+s
+print( "hello ",concatenate)
